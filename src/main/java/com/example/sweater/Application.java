dd2acceb036c2829abaeaf7d7a76f7eba2a5e540
Yevgeniy Yuzhaninov <jahdao@gmail.com>
2022-09-09 18:22:12 +0700

    Add new comment line

--- src/main/java/com/example/sweater/Application.java
+++ src/main/java/com/example/sweater/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     // I'm trying to understand what's happening here
+    // And one new comment
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
